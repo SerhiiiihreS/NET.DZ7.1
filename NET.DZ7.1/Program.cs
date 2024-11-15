4da027845e2f86db44f38c26039db9ab7acc7079
Serhii <0992830717s@gmail.com>
2024-11-15 18:20:27 +0100

    Add project files.

---
+++ NET.DZ7.1/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace NET.DZ7._1
+{
+    internal class Program
+    {
+        struct Fraction
+        {
+            int numerator;
+            int denominator;
+            public Fraction(int c, int z)
+            {
+                numerator = c;
+                denominator = z;
+            }
+
+            public void input()
+            {
+                numerator = Convert.ToInt32(Console.Read());
+                denominator = Convert.ToInt32(Console.Read());
+            }
+            public void mult(int x)
+            {
+                numerator *= x;
+                Console.WriteLine("Fraction:  " + numerator + "/" + denominator);
+            }
+            public void div(int x)
+            {
+                denominator *= x;
+                Console.WriteLine("Fraction:  " + numerator + "/" + denominator);
+            }
+
+            public void sum(int x)
+            {
+                numerator += (x * denominator);
+                Console.WriteLine("Fraction:  " + numerator + "/" + denominator);
+            }
+            public void sub(int x)
+            {
+                numerator -= (x * denominator);
+                Console.WriteLine("Fraction:  " + numerator + "/" + denominator);
+
+            }
+            public void show()
+            {
+                Console.WriteLine("Fraction:  " + numerator + "/" + denominator);
+            }
+            public void reduct(int x)
+            {
+                numerator /= x;
+                denominator /= x;
+            }
+
+        }
+
+
+        struct ComplexNumber
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction(2,3);
+            a.show();
+            a.sum(1);
+            a.div(2);
+            a.mult(3);
+        }
+    }
+}
